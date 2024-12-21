7573837bcb5903143a630054db9e57d019b4e24a
Endy1381 <119288346+Endy1381@users.noreply.github.com>
2024-12-21 16:37:04 +0300

    Update App.jsx
    added the TODO feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 import Logo from './assets/logo.png'
+import log from "eslint-plugin-react/lib/util/log.js";
 
 function App() {
-  const [todo, setTodo] = useState(0)
+  const [todos, setTodos] = useState([
+      {
+          id: 1,
+          name: "bruhh",
+          completed: false
+      }
+  ])
+    const input = useRef(null)
+    const lastTodo = todos.at(-1)
+    console.log(lastTodo)
+    function handleClick(e) {
+      e.preventDefault()
+      let newTodo = {
+          id: lastTodo['id'] + 1,
+          name: input.current.value,
+          completed: false
+      }
+      setTodos([...todos,newTodo])
+    }
 
   return (
     <>
@@ -16,8 +35,21 @@ function App() {
       </header>
 
     {/*  input  */}
-        <section>
-
+        <h1 className="text-3xl  mb-8 text-center">
+            insert your TODO here
+        </h1>
+        <section className="flex justify-center items-center">
+            <input type="text" ref={input} className="border px-5 py-3"/>
+            <button className="mx-5 text-white px-5 py-3 bg-emerald-800" onClick={handleClick}>Add</button>
+        </section>
+        <section className="flex justify-center items-center">
+            <ul>
+                {todos.map(todo => {
+                    return (
+                        <li key={todo.id}>{todo.name}, {todo.id}</li>
+                    )
+                })}
+            </ul>
         </section>
     </>
   )
